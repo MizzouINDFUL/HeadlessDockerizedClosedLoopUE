68ff5b46b498b5cd6e4b4a46267892002756f293
legibleguy <solovyovandy@yandex.ru>
2023-03-05 22:47:40 -0600

    Reverting changes from previous (accidental) commit

--- airsim-ros/shared/src/airsim_extractor.py
+++ airsim-ros/shared/src/airsim_extractor.py
@@ -81,7 +81,7 @@ class AirSimExtractor():
         cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
         curr_count = self.get_folder_count()
         name_og = get_image_name(curr_count)
-        name = topicFolder + name_og
+        name = self.topicFolder + name_og
 
         print("Saving the image #"+str(curr_count)+" to disk...")
         print("Image name: "+name)
